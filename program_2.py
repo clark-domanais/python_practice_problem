0f9d860372070301fcc77c3eed5bf33d2614013b
Clark Domanais <clarkdomanais@gmail.com>
2026-03-09 19:01:50 +0800

    init commit

---
+++ program_2.py
@@ -0,0 +1,9 @@
+#Ask the user to input 2 numbers
+num_1 = int(input("Please enter a number: "))
+num_2 = int(input("Please enter a number: "))
+
+#Determine if the 2 numbers that's been inputted are equal
+if num_1 == num_2:
+    print("Both numbers are equal")
+else:
+    print("Both numbers are not equal")
