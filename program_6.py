ca5e084ec3432b0f19c9f049a49f6b6f6d892292
Clark Domanais <clarkdomanais@gmail.com>
2026-03-09 19:12:59 +0800

    init commit

---
+++ program_6.py
@@ -0,0 +1,7 @@
+
+#Ask the user to input 2 numbers
+num_1 =  float(input("Please enter a number: "))
+num_2 = float(input("Please enter a number: "))
+
+#Print thhe result when the first number is raised to the power of the second number
+print("The result when the first number is raised to the power of the second number is: ", num_1 ** num_2)
