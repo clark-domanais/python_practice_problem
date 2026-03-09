71f107257e7d4509f8937cee05daac2367aac5e6
Clark Domanais <clarkdomanais@gmail.com>
2026-03-09 19:05:11 +0800

    init commit

---
+++ program_3.py
@@ -0,0 +1,7 @@
+
+#Ask the user to input 2 numbers
+num_1 =  int(input("Please enter a number: "))
+num_2 = int(input("Please enter a number: "))
+
+#Print the sum of the two numbers
+print("The sum of the two numbers is: ", num_1 + num_2)
