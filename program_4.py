286e4949f00b03c31788832fe13e9e967633f82c
Clark Domanais <clarkdomanais@gmail.com>
2026-03-09 19:07:14 +0800

    init commit

---
+++ program_4.py
@@ -0,0 +1,7 @@
+
+#Ask the user to input 2 numbers
+num_1 =  int(input("Please enter a number: "))
+num_2 = int(input("Please enter a number: "))
+
+#Print the product of the two numbers
+print("The product of the two numbers is: ", num_1 * num_2)
