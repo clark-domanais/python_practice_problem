f7d8815f923e56edf66923acf1b27a9b9e34a4e2
Clark Domanais <clarkdomanais@gmail.com>
2026-03-09 19:10:55 +0800

    init commit

---
+++ program_5.py
@@ -0,0 +1,10 @@
+
+#Ask the user to input 2 numbers
+num_1 =  float(input("Please enter a number: "))
+num_2 = float(input("Please enter a number: "))
+
+#Print the quotient of the two numbers with decimal point
+if num_2 != 0:
+    print("The quotient of the two numbers is: ", num_1 / num_2)
+else:
+    print("Error:12 Division by zero is not allowed.")
