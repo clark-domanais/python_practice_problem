db7c3ebf864886a381031c55f73866170aca90c2
Clark Domanais <clarkdomanais@gmail.com>
2026-03-18 23:26:24 +0800

    added an input variable where the system asks the user to input 2 numbers

---
+++ numbers_in_between.py
@@ -0,0 +1,6 @@
+
+#Ask the user to input 2 numbers
+num_1 = int(input("Please enter a number: "))
+num_2 = int(input("Please enter a number: "))
+
+#Print the numbers in between the 2 numbers that's been inputted
