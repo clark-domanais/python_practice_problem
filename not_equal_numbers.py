9be5666f8de0d3855fed932b577f2b565606796f
clark-domanais <clarkjosephddomanais@iskolarngbayan.pup.edu.ph>
2026-03-18 18:44:08 +0800

    Delete not_equal_numbers.py

--- not_equal_numbers.py
+++
@@ -1,8 +0,0 @@
-
-#Ask the user to input 2 numbers
-num_1 = int(input("Please enter a number: "))
-num_2 = int(input("Please enter a number: "))
-
-#Determine if the 2 numbers that's been inputted are not equal
-if num_1 != num_2:
-    print("Both numbers are not equal")
