869639e40e9b76fb63a15479bf9ad9d023061168
Clark Domanais <clarkdomanais@gmail.com>
2026-03-18 23:27:40 +0800

    added a condition where it prints the numbers in between the 2 numbers that has been inputted

--- numbers_in_between.py
+++ numbers_in_between.py
@@ -4,3 +4,6 @@ num_1 = int(input("Please enter a number: "))
 num_2 = int(input("Please enter a number: "))
 
 #Print the numbers in between the 2 numbers that's been inputted
+if num_1 < num_2:
+    for i in range(num_1 + 1, num_2):
+        print(i)
